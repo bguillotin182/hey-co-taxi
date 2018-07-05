ca2ac966b4dd776d3e4348f1358b3c3689f44fa2
benoit GUILLOTIN <benoit@lumapps.com>
2018-07-05 09:35:13 +0200

    fix(small): comment remove

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import store from './store';
 import Header from './header/header';
 import Mouse from './utils/Mouse';
-import Content3  from './three-test/three-test';
+//import Content3  from './three-test/three-test';
 
 import logo from './logo.svg';
 import './App.css';
@@ -12,13 +12,13 @@ class App extends Component {
 
   render() {
     const { x, y } = this.props;
-
+  //      <Content3 />
     return (
       <div className="App">
         <Header />
         <strong> X : {x} - Y : {y} </strong>
         <Mouse render={ ({x, y}) => <strong> X: {x} - Y : { y } </strong>} toto={ () => 'toto' } />
-        <Content3 />
+
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
