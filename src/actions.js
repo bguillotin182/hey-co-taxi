989e347a95c28bab07c2428089c1478ce0eee4bc
benoit GUILLOTIN <benoit@lumapps.com>
2018-07-05 09:42:07 +0200

    fix(master): fix new action

--- src/actions.js
+++ src/actions.js
@@ -2,3 +2,9 @@ export const changeMouseXY = (x, y) => ({
     type: 'MOUSE_CHANGE_XY',
     payload: { x, y },
 });
+
+
+export const changeMouseClickPositionXY = (x, y) => ({
+    type: 'MOUSE_CLICK_POSITION_XY',
+    payload: { x, y },
+})
